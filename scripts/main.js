f11a9a0317c50e15a43fa5193fd39f61ba71a9dd
Sephini123 <cigenewari@yahoo.co.uk>
2022-11-14 00:42:51 +0000

    <will get there eventually>

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,27 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
+var randomTopic = topics[Math.floor(Math.random() *topics.length)];
+console.log ('Here are the topics we learned through Prework');
+listTopics();
+function listTopics() {
 for (var x = 0; x <topics.length; x++){
     console.log(topics[x]);
 }
+}
 
-/*
-
-if (topic ==='HTML') {
+console.log('Which topic should we study first?');
+selectTopic();
+function selectTopic(){
+if (randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-}else if (topic === 'CSS'){
+}else if (randomTopic === 'CSS'){
     console.log("Let's study CSS!");
-}else if (topic ==='Git') {
+}else if (randomTopic === 'Git') {
     console.log("Let's study Git!");
-}else if (topic === 'JavaScript'){
+}else if (randomTopic === 'JavaScript'){
     console.log("Let's study JavaScript!"); 
 }else {
     console.log('Please try again!');
 }
-var shapes = ["triangles", "squares", "pentagon", "circle"];
-for (var x = 0; x < shapes.length; x++) {
-    console.log(shapes[x]);
 }
-*/
+listTopics()
+selectTopic()
